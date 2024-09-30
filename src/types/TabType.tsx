a203b87a545b18b7c5fb4255e000a73176c4dec7
AcidOP <zeeshanalivr46@gmail.com>
2024-09-30 14:09:25 +0530

    feat: custom types

---
+++ src/types/TabType.tsx
@@ -0,0 +1,6 @@
+import type { LucideIcon } from 'lucide-react';
+import type { IconType } from 'react-icons';
+
+type TabType = { name: string; url: string; icon: IconType | LucideIcon };
+
+export default TabType;
