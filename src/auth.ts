7551ec806c255d8f4296e7f807e333885d5adf53
AcidOP <zeeshanalivr46@gmail.com>
2024-09-30 20:36:40 +0530

    feat: configured prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,13 @@
+import { db } from '@/lib/db';
+
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  // trustHost: true,
-  // adapter: PrismaAdapter(prisma),
-  // secret: process.env.AUTH_SECRET,
+  trustHost: true,
+  adapter: PrismaAdapter(db),
+  secret: process.env.AUTH_SECRET,
   providers: [GitHub],
   debug: process.env.NODE_ENV === 'development',
 });
