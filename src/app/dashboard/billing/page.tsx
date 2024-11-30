7e5e463795907767b3f86b53b500ee1676f8b7b7
AcidOP <zeeshanalivr46@gmail.com>
2024-11-30 22:51:21 +0530

    feat: billing page

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,11 +1,74 @@
+import { Plan } from '@prisma/client';
+import Link from 'next/link';
+
+import { getUserId, getUserPlanFromUserId } from '@/server/session';
+
 import Heading from '@/components/DashboardHeading';
 
-const Billing = () => {
+import { Button } from '@/components/ui/button';
+
+const getUserPlan = async (): Promise<Plan> => {
+  const userId = await getUserId();
+  return userId ? await getUserPlanFromUserId(userId) : 'FREE';
+};
+
+interface PlanInfoProps {
+  plan: Plan;
+}
+
+const FreePlanInfo = () => {
   return (
-    <div>
-      <Heading text='Billing' />
+    <>
+      <p>
+        You are currently on the{' '}
+        <span className='font-bold text-primary'>&apos;FREE&apos;</span> plan which
+        supports up to <span className='underline'>3</span> testimonials and{' '}
+        <span className='underline'>limited</span> features.
+      </p>
+
+      <Link href='/pricing'>
+        <Button className='mt-4'>Upgrade Plan</Button>
+      </Link>
+    </>
+  );
+};
+
+const PaidPlanInfo = () => {
+  return (
+    <div className='text-lg'>
+      <p>
+        You are currently on the{' '}
+        <span className='font-bold text-primary'>&apos;PAID&apos;</span> plan which
+        supports up to <span className='underline'>unlimited</span> testimonials.
+      </p>
+
+      <div className='mt-8 space-y-2'>
+        <div className='flex gap-2'>
+          <h3>Amount Paid:</h3>
+          <p className='font-bold'>Full</p>
+        </div>
+        <div className='flex gap-2'>
+          <h3>Valid Till:</h3>
+          <p className='font-bold'>Forever</p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Billing;
+const PlanInfo = ({ plan }: PlanInfoProps) => {
+  return plan === 'FREE' ? <FreePlanInfo /> : <PaidPlanInfo />;
+};
+
+export default async function Billing() {
+  const plan = await getUserPlan();
+
+  return (
+    <>
+      <Heading text='Billing' />
+      <section className='mt-8'>
+        <PlanInfo plan={plan} />
+      </section>
+    </>
+  );
+}
--- src/server/session.ts
+++ src/server/session.ts
@@ -1,7 +1,9 @@
 import { cache } from 'react';
 
+import db from '@/lib/prisma';
 import { auth } from '@/lib/session';
 
+import type { Plan, User as PrismaUser } from '@prisma/client';
 import type { Session, User } from 'next-auth';
 
 export const getServerSession = cache(async (): Promise<Session | null> => {
@@ -16,4 +18,19 @@ export const getUserSession = cache(async (): Promise<User | null> => {
 export const getUserId = cache(async (): Promise<string | null> => {
   const session = await getServerSession();
   return session?.user?.id ?? null;
+});
+
+export const getUserPlanFromUserId = cache(async (userId: string): Promise<Plan> => {
+  let user: PrismaUser | null = null;
+
+  try {
+    user = (await db.user.findUnique({
+      where: { id: userId },
+      select: { plan: true },
+    })) as PrismaUser;
+  } catch (error) {
+    console.error(error);
+  }
+
+  return user?.plan ?? 'FREE';
 });
